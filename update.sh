2345506f44cfb25cec2d31e8d2dc16a4be4a1f9f
burdeyniyua <38028182+burdeyniyua@users.noreply.github.com>
2018-12-21 20:23:30 +0200

    Update "-y" to commands

--- update.sh
+++ update.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 #sudo passwd root
 sudo apt-get update
-sudo apt-get install pure-ftpd -y
-sudo apt-get install xrdp nmap mtr console-cyrillic
-sudo apt-get install console-cyrillic mc htop -y
+sudo apt-get install pure-ftpd xrdp nmap mtr console-cyrillic mc htop -y
+sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
-sudo apt-get -y upgrade
 sudo rpi-update
 sudo apt-get autoremove -y
 sudo apt-get autoclean -y
